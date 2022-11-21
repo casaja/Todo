89a9fcc3e059485419e6751bdaf14a29265e4e31
Javier SC <jsanchez@ayesa.com>
2022-11-21 15:30:39 +0100

    Test Service findAll

--- src/main/java/net/jsc/example/todo/ToDo.java
+++ src/main/java/net/jsc/example/todo/ToDo.java
@@ -1,16 +1,51 @@
 package net.jsc.example.todo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class ToDo {
-    private final long id;
-    private final String text;
-    private final boolean completed;
 
+    @Id
+    @GeneratedValue
+    private long id;
+    private String text;
+    private boolean completed;
+
+    public ToDo() {}
+
+    public ToDo(String text, boolean completed) {
+        this.text = text;
+        this.completed = completed;
+    }
     public ToDo(long id, String text, boolean completed) {
         this.id = id;
         this.text = text;
+        this.completed = completed;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public boolean isCompleted() {
+        return this.completed;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setCompleted(boolean completed) {
         this.completed = completed;
     }
 }
--- src/main/java/net/jsc/example/todo/ToDoService.java
+++ src/main/java/net/jsc/example/todo/ToDoService.java
@@ -1,12 +1,25 @@
 package net.jsc.example.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ToDoService {
-    public List<ToDo> findAll() {
-        return null;
+
+    private ToDoRepository toDoRepository;
+
+    public ToDoService(ToDoRepository toDoRepository) {
+        this.toDoRepository = toDoRepository;
     }
+
+    public List<ToDo> findAll() {
+        return this.toDoRepository.findAll();
+    }
+
+//    public ToDo save(ToDo toDo) {
+//        return toDoRepository.save(toDo);
+//    }
 }
